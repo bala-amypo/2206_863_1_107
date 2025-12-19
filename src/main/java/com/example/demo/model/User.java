a7cc3b95984ce11703bff265c953dcd053bdca92
bala-amypo <bala@amypo.in>
2025-12-19 16:17:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,51 @@
+    package com.example.demo.model;
+
+    import jakarta.persistence.*;
+
+    @Entity
+    @Table(name = "users")
+    public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, String role) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+    }
+
+    public Long getId() {
+    return id;
+    }
+
+    public String getEmail() {
+    return email;
+    }
+
+    public String getRole() {
+    return role;
+    }
+
+    public void setEmail(String email) {
+    this.email = email;
+    }
+
+    public void setRole(String role) {
+    this.role = role;
+    }
+    }
