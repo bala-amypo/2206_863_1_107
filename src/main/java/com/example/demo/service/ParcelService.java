ece40556c8f1089996396d64be1c485bbf26b6a0
bala-amypo <bala@amypo.in>
2025-12-19 17:01:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ParcelService.java
@@ -0,0 +1,8 @@
+import com.example.demo.model.Parcel;
+
+public interface ParcelService {}
+
+    Parcel addParcel(Parcel parcel);
+
+        Parcel getByTrackingNumber(String trackingNumber);
+        }
---
+++ src/main/java/com/example/demo/service/service.impl/ClaimRuleServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.model.ClaimRule;
+import com.example.demo.repository.ClaimRuleRepository;
+import com.example.demo.service.ClaimRuleService;
+import java.util.List;
+
+public class ClaimRuleServiceImpl implements ClaimRuleService {
+
+private final ClaimRuleRepository ruleRepository;
+
+public ClaimRuleServiceImpl(ClaimRuleRepository ruleRepository) {
+this.ruleRepository = ruleRepository;
+}
+
+public ClaimRule addRule(ClaimRule rule) {
+if (rule.getWeight() < 0) {
+throw new BadRequestException("weight invalid");
+}
+return ruleRepository.save(rule);
+}
+
+public List<ClaimRule> getAllRules() {
+return ruleRepository.findAll();
+}
+}
