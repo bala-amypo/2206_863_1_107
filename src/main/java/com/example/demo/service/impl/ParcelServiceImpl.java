180b82a01f66803ff818bfdcf18ade6642c5edd4
bala-amypo <bala@amypo.in>
2025-12-20 11:28:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ParcelServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ParcelServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.*;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Parcel;
 import com.example.demo.repository.ParcelRepository;
 import com.example.demo.service.ParcelService;
@@ -17,21 +18,27 @@ public ParcelServiceImpl(ParcelRepository repo) {
 this.repo = repo;
 }
 
+@Override
 public Parcel add(Parcel parcel) {
-if (parcel.getWeightKg() <= 0) {}
+if (parcel.getWeightKg() == null || parcel.getWeightKg() <= 0) {
 throw new BadRequestException("weight invalid");
 }
+
 if (repo.existsByTrackingNumber(parcel.getTrackingNumber())) {
 throw new BadRequestException("tracking exists");
 }
+
 return repo.save(parcel);
 }
 
+@Override
 public Parcel getByTracking(String tracking) {
 return repo.findByTrackingNumber(tracking)
 .orElseThrow(() -> new ResourceNotFoundException("parcel not found"));
 }
 
+@Override
 public List<Parcel> getAll() {
 return repo.findAll();
+}
 }
